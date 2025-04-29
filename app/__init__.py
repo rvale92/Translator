d9b69b6bc18ccaa5595034aefea93280c146763e
rvale92 <rvale92@users.noreply.github.com>
2025-04-29 01:34:12 +0000

    deploy: a150e3a1a4759c24fdedc4d533faa4a7251e8451

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Voice Translation App package."""
-
-from .config import SUPPORTED_LANGUAGES, INPUT_DIR, OUTPUT_DIR
-from .stt import default_stt
-from .translator import default_translator
-from .tts import default_tts
-from .utils import cleanup_old_files 
